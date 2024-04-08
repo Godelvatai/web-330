e2b2bae41fea5efff276b11dfc936df247250a1b
Godelvatai <godelvatai@gmail.com>
2024-04-08 00:15:02 -0400

    Week 3 HW Upload
    Week 3 HW Upload

--- week-3/project09-02/project09-02a_txt.js
+++ week-3/project09-02/project09-02a_txt.js
@@ -4,8 +4,8 @@
       Project 09-02
 
       Project to read field values from web storage
-      Author: 
-      Date:   
+      Author: Dustin Craven
+      Date:   4/7/24
 
       Filename: project09-02a.js
 */
@@ -20,3 +20,19 @@ let accOption = document.getElementById("accOption");
 let region = document.getElementById("region");
 let miles = document.getElementById("miles");
 let comments = document.getElementById("comments");
+
+function showData() {
+  sessionStorage.setItem("riderName", riderName.value);
+  sessionStorage.setItem("ageGroup", ageGroup.value);
+  sessionStorage.setItem("bikeOption", bikeOption.value);
+  sessionStorage.setItem("routeOption", routeOption.value);
+  sessionStorage.setItem("accOption", accOption.value);
+  sessionStorage.setItem("region", region.value);
+  sessionStorage.setItem("miles", miles.value);
+  sessionStorage.setItem("comments", comments.value);
+
+  location.href = "project09-02b_txt.html";
+  
+}
+
+document.getElementById("submitButton").onclick = showData;
