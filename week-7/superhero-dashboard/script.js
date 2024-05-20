5368fd9d7c311bf85d27e7f0d067f6baacdf4535
Godelvatai <godelvatai@gmail.com>
2024-05-19 20:36:21 -0400

    Week 7and8 HW Push
    Week 7and8 HW Push

--- week-7/superhero-dashboard/script.js
+++ week-7/superhero-dashboard/script.js
@@ -3,27 +3,98 @@
   Chapter 3
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author:    Dustin Craven
+  Date:      5/19/24
+  Filename:  script.js
 */
 
 "use strict";
 
 let heroes = [
-  // TODO: Create superhero objects
+  //Create superhero objects
+  {name: "Superman",superpower: "Kryptonian",weakness: "kryptonite",city: "Metropolis"},
+  {name: "Martian Manhunter",superpower: "Martian",weakness: "fire",city: "Middleton"},
+  {name: "Flash",superpower: "Speed Force",weakness: "cold temperatures",city: "Central City"}
 ];
 
 function fetchHero1() {
-  // TODO: Implement this function
+  //return promise for the retrieval of information for hero 1
+  return new Promise((resolve, reject) => {
+    //Two second timeout delay
+    setTimeout(() => {
+      //For testing, Math.random() < 0.9 causes Promise to be rejected 10% of the time
+      if(Math.random() < 0.9) {
+        //resolve promise with first hero of heroes array
+        resolve(heroes[0]);
+      } else {
+        //reject with message that retrieval of info failed
+        reject(`Unable to retrieve information for ${heroes[0].name}`);
+      }
+    }, 2000);
+  });
 }
 
 function fetchHero2() {
-  // TODO: Implement this function
+  //return promise for the retrieval of information for hero 2
+  return new Promise((resolve, reject) => {
+    //Five second timeout delay
+    setTimeout(() => {
+      //For testing, Math.random() < 0.9 causes Promise to be rejected 10% of the time
+      if(Math.random() < 0.9) {
+        //resolve promise with the second hero of heroes array
+        resolve(heroes[1]);
+      } else {
+        //reject with message that retrieval of info failed
+        reject(`Unable to retrieve information for ${heroes[1].name}`);
+      }
+    }, 5000);
+  });
 }
 
 function fetchHero3() {
-  // TODO: Implement this function
+  //return promise for the retrieval of information for hero 3
+  return new Promise((resolve, reject) => {
+    //Three second timeout delay
+    setTimeout(() => {
+      //For testing, Math.random() < 0.9 causes Promise to be rejected 10% of the time
+      if(Math.random() < 0.9) {
+        //resolve promise with the third hero of heroes array
+        resolve(heroes[2]);
+      } else {
+        //reject with message that retrieval of info failed
+        reject(`Unable to retrieve information for ${heroes[2].name}`);
+      }
+    }, 3000);
+  });
 }
 
 // TODO: Use Promise.allSettled to fetch all heroes and update the webpage
+Promise.allSettled([fetchHero1(), fetchHero2(), fetchHero3()]).then((results) => {
+  results.forEach((result) => {
+    if(result.status === "fulfilled") {
+      let resultHero = result.value;
+      let formattedOutput = `Superhero Name: ${resultHero.name}<br>Super Power: ${resultHero.superpower}<br>Weakness: ${resultHero.weakness}<br>Home City: ${resultHero.city}`;
+      switch(resultHero.name) {
+        case "Superman":
+          document.getElementById("hero1").innerHTML = formattedOutput;
+          break;
+        case "Martian Manhunter":
+          document.getElementById("hero2").innerHTML = formattedOutput;
+          break;
+        case "Flash":
+          document.getElementById("hero3").innerHTML = formattedOutput;
+          break;
+        default:
+          break;
+      }
+    } else {
+      if(result.reason.includes("Superman")) {
+        document.getElementById("hero1Error").innerText = result.reason;
+      } else if(result.reason.includes("Martian Manhunter")) {
+        document.getElementById("hero2Error").innerText = result.reason;
+      } else if(result.reason.includes("Flash")) {
+        document.getElementById("hero3Error").innerText = result.reason;
+      }
+    }
+  })
+})
--- week-8/movie-app/script.js
+++ week-8/movie-app/script.js
@@ -3,21 +3,92 @@
   Chapter 4
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author:   Dustin Craven
+  Date:     5/19/24
+  Filename: script.js
 */
 
 "use strict";
 
 const movies = [
-  // Add your movie objects here
+  {
+    title: "The Lord of the Rings: Return of the King",
+    year: "2003",
+    director: "Peter Jackson",
+    synopsis: "Hobbit destroys shiny ring in volcano and saves world from evil."
+  },
+  {
+    title: "The Hobbit: An Unexpected Journey",
+    year: "2012",
+    director: "Peter Jackson",
+    synopsis: "Hobbit is visited by old man in robes and is suddenly on a dangerous adventure."
+  },
+  {
+    title: "Train to Busan",
+    year: "2016",
+    director: "Yeon Sang-ho",
+    synopsis: "Zombie outbreak forces strangers on a train to unite and survive until the train stops and they realize the outbreak is widespread."
+  },
+  {
+    title: "The Matrix",
+    year: "1999",
+    director: "Lana & Lilly Wachowskis",
+    synopsis: "Man breaks out of a false reality and participates in the rebellion against intelligent machines that are controlling humanity."
+  },
+  {
+    title: "How to Train Your Dragon",
+    year: "2012",
+    director: "Chris Sanders & Dean DeBlois",
+    synopsis: "Kid bonds with dragon after shooting it from sky and injuring it and ends a multi-year feud between the dragons and his people."
+  }
 ];
 
 function fetchMovie(title) {
-  // Implement this function
+  return new Promise((resolve, reject) => {
+    const searchResult = movies.find(movie => movie.title === title);
+
+    setTimeout(() => {
+      if(!searchResult) {
+        reject("Movie not found");
+      } else {
+        resolve(searchResult);
+      }
+    }, 2000);
+  });
 }
 
 document.getElementById("movie-form").addEventListener("submit", async (event) => {
-  // Implement this function
+  //Store HTML elements in variables for easier use
+  let errorMessage = document.getElementById("error-message");
+  let movieTitle = document.getElementById("movie-title");
+  let movieDirector = document.getElementById("movie-director");
+  let movieYear = document.getElementById("movie-year");
+  let movieSynopsis = document.getElementById("movie-synopsis");
+  let searchValue = document.getElementById("title-input");
+  
+  //Reset page on new search
+  errorMessage.innerText = "";
+  movieTitle.innerText = "";
+  movieDirector.innerText = "";
+  movieYear.innerText = "";
+  movieSynopsis.innerText = "";
+
+  //Attempt to call fetchMovie() with try catch block
+  try {
+    //Stop form from submitting
+    event.preventDefault();
+
+    console.log(searchValue.value);
+    //Use await to pull result of fetchMovie() and store value in variable
+    let searchResult = await fetchMovie(searchValue.value);
+
+    //Set search result values to page
+    movieTitle.innerText = searchResult.title;
+    movieDirector.innerText = searchResult.director;
+    movieYear.innerText = searchResult.year;
+    movieSynopsis.innerText = searchResult.synopsis;
+  } catch(err) {
+    //Set error message value to page
+    errorMessage.innerText = err;
+  }
 });
